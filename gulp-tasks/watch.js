0484ff5b9d4a2d26eb073c6b1969541036f24755
bradyhullopeter <brady.hullopeter@gmail.com>
2016-04-06 16:52:51 -0700

    cleanup

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -1,8 +1,9 @@
 module.exports = function (gulp, plugins) {
     return function () {
-        gulp.watch('app/**/*.js', ['app-js']);
-        gulp.watch('app/**/*.html' ['html']);
-        gulp.watch('app/**/*.scss', ['app-styles']);
-        gulp.watch('app/images/**/*', ['images']);
+        gulp.watch('./app/**/*.js', ['app-js']);
+        gulp.watch('./app/**/*.html' ['html']);
+        gulp.watch('./app/**/*.scss', ['app-styles']);
+        gulp.watch('./app/images/**/*', ['images']);
+        gulp.watch('./app/fonts/**/*', ['fonts']);
     }
 }
