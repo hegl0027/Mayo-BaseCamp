3bf7fad1b2056d76783cfe8d4ea37e5054c5c70d
bradyhullopeter <brady.hullopeter@gmail.com>
2016-08-01 19:55:18 -0500

    remove random views

--- app/views/architecture/architecture-controller.js
+++
@@ -1,6 +0,0 @@
-export default class ArchitectureController {
-    constructor() {
-        this.name = 'Architecture Controller';
-        this.salutation = 'Hi from the Architecture :)';
-    }
-}
