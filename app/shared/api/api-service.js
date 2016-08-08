db4008ee9adc42df47c3f19f11b27f2fbbb423c4
bradyhullopeter <brady.hullopeter@gmail.com>
2016-08-08 09:41:38 -0500

    update test case for api service

--- app/shared/api/api-service.js
+++ app/shared/api/api-service.js
@@ -1,12 +1,12 @@
-'use strict';
 
 /**
- * @description The 'api' is an angular service that facilitates mapping RESTful endpoints via ngResource.
- * @param $http
- * @param $q
- * @param $resource
+ * @module app.shared.api
+ * @description - The 'api' is an angular service that facilitates mapping RESTful endpoints via ngResource.
+ * @param {Object} $http - Angular's built-in $http service
+ * @param {Object} $q - Angular's built-in $q service
+ * @param {Object} $resource - Angular's optional $resource service
  *
- * @returns {{User: *}} Object literal representing the associated REST endpoint entities.
+ * @returns {{User: *}} - Object literal representing the associated REST endpoint entities and actions available.
  */
 export default function ApiService($http, $q, $resource) {
     return {
