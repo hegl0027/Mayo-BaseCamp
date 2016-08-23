54acc34f047dfc9f3d13fedd03dc17b63d8e5342
bradyhullopeter <brady.hullopeter@gmail.com>
2016-08-23 09:06:54 -0500

    cleanup

--- app/shared/nav-select/nav-select.spec.js
+++ app/shared/nav-select/nav-select.spec.js
@@ -16,13 +16,17 @@ describe('nav-select directive ', () => {
     scope.$digest();
   }));
 
-  it('should remove sibling selected class on click', () => {
+  it('should be setup correctly', () => {
     let $li = element.find('li');
     expect($li).to.have.lengthOf(2);
     expect($li.eq(0).hasClass('selected')).to.be.true;
+    expect($li.eq(1).hasClass('selected')).to.be.false;
+  });
 
+  it('should remove sibling selected class on click', () => {
+    let $li = element.find('li');
     $li.eq(1).triggerHandler('click');
-    expect(element.find('li').eq(0).hasClass('selected')).to.be.false;
+    expect($li.eq(0).hasClass('selected')).to.be.false;
     expect($li.eq(1).hasClass('selected')).to.be.true;
   });
 
