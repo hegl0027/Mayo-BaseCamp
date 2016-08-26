7b7736d3e9b28f85c0619bb10cb6377cdf7d66f1
bradyhullopeter <brady.hullopeter@gmail.com>
2016-08-26 13:16:08 -0500

    remove erroneous test case

--- e2e-tests/home/home.spec.js
+++ e2e-tests/home/home.spec.js
@@ -1,31 +1,17 @@
 import HomePage from './home.page';
-import $ from 'jquery';
-import http from 'http'
+
 let page;
 
 describe('app home', () => {
 
   beforeEach(function () {
     page = new HomePage();
-
-    http.request({
-      auth: 'user:password',
-      path: 'sample/sample.json'
-    }, (res) => {
-      console.log(res);
-    }).end();
   });
 
   describe('empty fragment redirect', () => {
     it('should automatically redirect to /app/home/one when location hash/fragment is empty', () => {
       expect(browser.getLocationAbsUrl()).toMatch("/app/home/one");
     });
-  });
-
-  describe('button', () => {
-    it('should have a button', () => {
-      expect(page.button.getText()).toEqual('Button');
-    })
   });
 
 });
