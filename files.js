7f94fc19c0519e583104656c13e4d3a12e98e347
Brady Hullopeter <hullopeter.brady@mayo.edu>
2016-12-21 20:57:44 -0600

    style changes

--- files.js
+++ files.js
@@ -8,7 +8,6 @@ const COMPONENTS_DIR = `${APP_DIR}/components`;
 const APP_SPEC_SRC = `${APP_DIR}/**/*.spec.js`;
 const E2E_SPEC_SRC = 'e2e-tests/**/*.spec.js';
 const BUILD_SCRIPT = 'gulpfile.babel.js';
-const SASS_SRC = `${ASSETS_DIR}/**/*.scss`;
 const HTML_SRC = `${APP_DIR}/**/*.html`;
 const ANGULAR_PARTIALS = `${APP_DIR}/components/**/*.html`;
 const FONTS_SRC = `${ASSETS_DIR}/fonts/**/*!(.txt)`;
@@ -17,48 +16,53 @@ const SVG_SRC = `${ASSETS_DIR}/images/svg-bundle/**/*.svg`;
 const INLINE_SVG_SRC = `${ASSETS_DIR}/images/inline-svg/**/*.svg`;
 
 const IMAGES_SRC = [
-    `${ASSETS_DIR}/images/**/*`,
-    `!${ASSETS_DIR}/images/inline-svg/`,
-    `!${ASSETS_DIR}/images/svg-bundle/`,
-    `!${SVG_SRC}`,
-    `!${INLINE_SVG_SRC}`
+  `${ASSETS_DIR}/images/**/*`,
+  `!${ASSETS_DIR}/images/inline-svg/`,
+  `!${ASSETS_DIR}/images/svg-bundle/`,
+  `!${SVG_SRC}`,
+  `!${INLINE_SVG_SRC}`
+];
+
+const SASS_SRC = [
+  `${ASSETS_DIR}/**/*.scss`,
+  `${APP_DIR}/**/*.scss`
 ];
 
 const APP_JS = [
-    `${APP_DIR}/**/*.js`,
-    `!${COMPONENTS_DIR}/templates.js`
+  `${APP_DIR}/**/*.js`,
+  `!${COMPONENTS_DIR}/templates.js`
 ];
 
 const JS_SRC = [
-    `${APP_DIR}/**/*.js`,
-    `!${APP_DIR}/**/*.spec.js`,
-    `!${COMPONENTS_DIR}/templates.js`
+  `${APP_DIR}/**/*.js`,
+  `!${APP_DIR}/**/*.spec.js`,
+  `!${COMPONENTS_DIR}/templates.js`
 ];
 
 const ALL_JS = [
-    `${APP_DIR}/**/*.js`,
-    `${E2E_SPEC_SRC}`
+  `${APP_DIR}/**/*.js`,
+  `${E2E_SPEC_SRC}`
 ];
 
 const TODO_SRC = [
-    `${ALL_JS}`,
-    `${TEST_CONFIGS_DIR}/**/*.js`,
-    `./*.js`
+  `${ALL_JS}`,
+  `${TEST_CONFIGS_DIR}/**/*.js`,
+  `./*.js`
 ];
 
 const SIZE_SRC = [
-    `${DEST_DIR}/**/*`,
-    `!${DEST_DIR}/images/favicon/**/*`, // ignore favicons
-    `!${DEST_DIR}/components/**/*`, // ignore views since they are bundled with app.js
-    `!${DEST_DIR}/**/*.map`, // ignore map files
-    `!${DEST_DIR}/**/app.js`, // ignore unminified assets
-    `!${DEST_DIR}/**/app.css`, // ignore unminified assets
-    `!${DEST_DIR}/fonts/**/*` // ignore; only a small subset of fonts will be pulled in
+  `${DEST_DIR}/**/*`,
+  `!${DEST_DIR}/images/favicon/**/*`, // ignore favicons
+  `!${DEST_DIR}/components/**/*`, // ignore views since they are bundled with app.js
+  `!${DEST_DIR}/**/*.map`, // ignore map files
+  `!${DEST_DIR}/**/app.js`, // ignore unminified assets
+  `!${DEST_DIR}/**/app.css`, // ignore unminified assets
+  `!${DEST_DIR}/fonts/**/*` // ignore; only a small subset of fonts will be pulled in
 ];
 
 const SASS_IGNORE_RESET = [
-    `${SASS_SRC}`,
-    `!${ASSETS_DIR}/**/*normalize.scss`
+  `${SASS_SRC}`,
+  `!${ASSETS_DIR}/**/*normalxize.scss`
 ];
 
 const EXTERNALS = [
@@ -76,39 +80,39 @@ const EXTERNALS = [
 ];
 
 export default {
-    src: {
-        dir: APP_DIR,
-        js: JS_SRC,
-        partials: ANGULAR_PARTIALS,
-        images: IMAGES_SRC,
-        svg: SVG_SRC,
-        inlineSvg: INLINE_SVG_SRC,
-        fonts: FONTS_SRC,
-        html: HTML_SRC,
-        e2e: E2E_SPEC_SRC,
-        sass: SASS_SRC,
-        unitTests: APP_SPEC_SRC,
-        size: SIZE_SRC,
-        allJs: ALL_JS,
-        entry: ENTRY,
-        sassIgnoreReset: SASS_IGNORE_RESET,
-        appJs: APP_JS,
-        todo: TODO_SRC
-    },
-    dest: {
-        dir: DEST_DIR,
-        images: `${DEST_DIR}/images`,
-        fonts: `${DEST_DIR}/fonts`,
-        html: `${DEST_DIR}`,
-        css: `${DEST_DIR}/css`,
-        components: `${COMPONENTS_DIR}`,
-        js: `${DEST_DIR}/js`,
-        docs: `${DOCS_DIR}`,
-        plato: `${REPORTS_DIR}/plato`
-    },
-    configs: {
-        scssLint: '.scsslint.yml'
-    },
-    buildScript: BUILD_SCRIPT,
-    externals: EXTERNALS
+  src: {
+    dir: APP_DIR,
+    js: JS_SRC,
+    partials: ANGULAR_PARTIALS,
+    images: IMAGES_SRC,
+    svg: SVG_SRC,
+    inlineSvg: INLINE_SVG_SRC,
+    fonts: FONTS_SRC,
+    html: HTML_SRC,
+    e2e: E2E_SPEC_SRC,
+    sass: SASS_SRC,
+    unitTests: APP_SPEC_SRC,
+    size: SIZE_SRC,
+    allJs: ALL_JS,
+    entry: ENTRY,
+    sassIgnoreReset: SASS_IGNORE_RESET,
+    appJs: APP_JS,
+    todo: TODO_SRC
+  },
+  dest: {
+    dir: DEST_DIR,
+    images: `${DEST_DIR}/images`,
+    fonts: `${DEST_DIR}/fonts`,
+    html: `${DEST_DIR}`,
+    css: `${DEST_DIR}/css`,
+    components: `${COMPONENTS_DIR}`,
+    js: `${DEST_DIR}/js`,
+    docs: `${DOCS_DIR}`,
+    plato: `${REPORTS_DIR}/plato`
+  },
+  configs: {
+    scssLint: '.scsslint.yml'
+  },
+  buildScript: BUILD_SCRIPT,
+  externals: EXTERNALS
 };
