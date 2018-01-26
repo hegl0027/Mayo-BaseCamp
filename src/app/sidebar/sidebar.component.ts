a777bb568b5df6687081a8ad49d4380ac52242cd
David McGaa <mcgaa.david@mayo.edu>
2018-01-26 10:02:48 -0800

    Added sidebar nav for patient link

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as DefaultUserPicture from '!svg-sprite-loader!mayo-ui-kit/assets/icons/font-awesome/user-circle.svg';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  public patientPicture = DefaultUserPicture;
 
   constructor() { }
 
